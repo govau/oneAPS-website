b625537541e956ca48fbd10d08fe49fe513cd04c
malmas18 <29763502+malmas18@users.noreply.github.com>
2021-04-07 12:17:17 +1000

    update opportunity table

--- api/Services/Entities/Opportunity.cs
+++ api/Services/Entities/Opportunity.cs
@@ -3,13 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Dta.OneAps.Api.Services.Entities
-{
+namespace Dta.OneAps.Api.Services.Entities {
     [Table("opportunity")]
-    public partial class Opportunity
-    {
-        public Opportunity()
-        {
+    public partial class Opportunity {
+        public Opportunity() {
             OpportunityAssessor = new HashSet<OpportunityAssessor>();
             OpportunityClarificationQuestion = new HashSet<OpportunityClarificationQuestion>();
             OpportunityHistory = new HashSet<OpportunityHistory>();
@@ -22,28 +19,69 @@ namespace Dta.OneAps.Api.Services.Entities
         [Key]
         [Column("id")]
         public int Id { get; set; }
-        [Column("framework_id")]
-        public int FrameworkId { get; set; }
-        [Column("lot_id")]
-        public int LotId { get; set; }
-        [Column("data", TypeName = "json")]
-        public string Data { get; set; }
-        [Column("created_at")]
-        public DateTime CreatedAt { get; set; }
-        [Column("updated_at")]
-        public DateTime UpdatedAt { get; set; }
-        [Column("published_at")]
-        public DateTime? PublishedAt { get; set; }
-        [Column("withdrawn_at")]
-        public DateTime? WithdrawnAt { get; set; }
-        [Column("domain_id")]
-        public int? DomainId { get; set; }
-        [Column("closed_at")]
-        public DateTime? ClosedAt { get; set; }
-        [Column("questions_closed_at")]
-        public DateTime? QuestionsClosedAt { get; set; }
-        [Column("responses_zip_filesize")]
-        public long? ResponsesZipFilesize { get; set; }
+
+        [Required]
+        [Column("job_title")]
+        public string JobTitle { get; set; }
+
+        [Column("job_description")]
+        public string JobDescription { get; set; }
+
+        [Column("what_you_gain")]
+        public string WhatYoullGain { get; set; }
+
+        [Column("about_team")]
+        public string AboutTeam { get; set; }
+
+        [Column("number_of_people")]
+        public string NumberOfPeople { get; set; }
+
+        [Column("start_date")]
+        public DateTime StartDate { get; set; }
+
+        [Column("end_date")]
+        public DateTime EndDate { get; set; }
+
+        [Column("commitment_time")]
+        public string CommitmentTime { get; set; }
+
+        [Column("agency")]
+        public string Agency { get; set; }
+
+        [Column("contact_person_name")]
+        public string ContactPersonName { get; set; }
+
+        [Column("contact_person_phone")]
+        public string ContactPersonPhone { get; set; }
+
+        [Column("location")]
+        public string Location { get; set; }
+
+        [Column("skills")]
+        public string Skills { get; set; }
+
+        [Column("additional_info")]
+        public string AdditionalInfo { get; set; }
+
+        [Column("security_clearance ")]
+        public string SecurityClearance { get; set; }
+
+        [Column("modifed")]
+        public DateTime? Modifed { get; set; }
+
+        [Column("created")]
+        public DateTime Created { get; set; }
+
+        [Required]
+        [Column("created_by")]
+        public string CreatedBy { get; set; }
+
+        [Column("modified_by")]
+        public string ModifiedBy { get; set; }
+
+
+        [InverseProperty("Opportunity")]
+        public virtual ICollection<OpportunitySkill> OpportunitySkills { get; set; }
 
         [InverseProperty("Opportunity")]
         public virtual ICollection<OpportunityAssessor> OpportunityAssessor { get; set; }

3def60d2223428d759f104a4838d06fbc325d28e
malmas18 <29763502+malmas18@users.noreply.github.com>
2021-04-12 09:38:11 +1000

    fix empty files

---
+++ client/src/pages/help-pages/digital-professionals/how-to-withdraw-application.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import DefaultLayout from "../../../components/layouts/default-layout";
+import { PageContext } from "../../../types/types";
+
+// markup
+const HowToWithdrawApplication: React.FC<PageContext> = ({ pageContext, location }) => {
+  return (
+    <DefaultLayout pageContext={pageContext} location={location}>
+      <>
+      </>
+    </DefaultLayout>
+  );
+};
+
+export default HowToWithdrawApplication;

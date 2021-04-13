e56538c60926b525266f623c1260547cdfae8cb6
malmas18 <29763502+malmas18@users.noreply.github.com>
2021-04-13 12:26:22 +1000

    logout working

--- client/src/pages/logout.tsx
+++ client/src/pages/logout.tsx
@@ -1,10 +1,27 @@
-import { Link } from "gatsby";
-import * as React from "react";
+import { navigate } from "gatsby";
+import React, { useContext } from "react";
 import DefaultLayout from "../components/layouts/default-layout";
 import SEO from "../components/seo";
 import { PageContext } from "../types/types";
+import { UserContext } from "../context/UserContext";
 
-// markup
+
+const LogoutButton: React.FC = () => {
+  const user = useContext(UserContext);
+  return (
+    <button
+      className="au-btn"
+      onClick={(e) => {
+        e.preventDefault();
+        user.updateToken();
+        navigate("/");
+      }}
+    >
+      Logout
+    </button>
+  );
+}
+
 const LogoutPage: React.FC<PageContext> = ({ pageContext, location }) => {
   return (
     <DefaultLayout pageContext={pageContext} location={location}>
@@ -14,9 +31,7 @@ const LogoutPage: React.FC<PageContext> = ({ pageContext, location }) => {
         <div className="container-fluid au-body">
           <p>Are you sure you want to logout? </p>
           <br />
-          <Link to="../#" className="au-btn">
-            Logout
-          </Link>
+          <LogoutButton />
         </div>
       </>
     </DefaultLayout>

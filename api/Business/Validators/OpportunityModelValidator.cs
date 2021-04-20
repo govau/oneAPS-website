8595753d7bf8c5cfd66d2d765e50101d17e334b5
malmas18 <29763502+malmas18@users.noreply.github.com>
2021-04-20 12:13:36 +1000

    add validator

--- api/Business/Validators/OpportunityModelValidator.cs
+++ api/Business/Validators/OpportunityModelValidator.cs
@@ -1,12 +1,23 @@
 using FluentValidation;
 using Dta.OneAps.Api.Business.Models;
+using System;
 
 namespace Dta.OneAps.Api.Business.Validators {
     public class OpportunityModelValidator : AbstractValidator<OpportunitySaveRequest> {
         public OpportunityModelValidator() {
             RuleFor(u => u.JobTitle).NotEmpty();
-
-            // RuleFor(u => u.AboutTeam).NotEmpty().When(o => o.);
+            RuleFor(u => u.JobDescription).NotEmpty();
+            RuleFor(u => u.WhatYoullGain).NotEmpty();
+            RuleFor(u => u.AboutTeam).NotEmpty();
+            RuleFor(u => u.NumberOfPeople).NotEmpty();
+            RuleFor(u => u.Location).NotEmpty();
+            RuleFor(u => u.Skills).NotEmpty();
+            RuleFor(u => u.StartDate).NotEmpty().GreaterThanOrEqualTo(DateTime.Now.Date);
+            RuleFor(u => u.EndDate).NotEmpty().GreaterThan(_ => _.StartDate);
+            RuleFor(u => u.Agency).NotEmpty();
+            RuleFor(u => u.ContactPersonName).NotEmpty();
+            RuleFor(u => u.ContactPersonPhone).NotEmpty();
+            RuleFor(u => u.SecurityClearance).NotEmpty();
         }
     }
 }

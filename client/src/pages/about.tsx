12764a9212af3de06e203217ac60ae5addefdb6c
malmas18 <29763502+malmas18@users.noreply.github.com>
2021-05-19 15:51:22 +1000

    update about content

--- client/src/pages/about.tsx
+++ client/src/pages/about.tsx
@@ -22,28 +22,37 @@ const AboutPage: React.FC<PageContext> = ({ pageContext, location }) => {
         <div className="col-sm-12 col-md-6 col-md-push-6 intro__img">
           <img
             className="au-responsive-media-img"
-            src="../../about-oneAPS.png"
+            src="/about-oneAPS.png"
             alt="Five people explaining ideas on a board"
           ></img>
         </div>
         <div className="col-sm-12 col-md-6 col-md-pull-6">
           <h2>What oneAPS is</h2>
           <p>
-            As part of the Digital Profession, our Digital Squads team are
-            trialing a whole-of-government program called oneAPS
-            Opportunities. This program aims to assist agencies to come
-            together to work on governemnt priorities and uplift digital
-            capabilities through digital talent mobility.
+            As part of the Digital Profession, our Digital Squads team are trialing a whole-of-government program called oneAPS Opportunities (working title). This program aims to assist agencies to come together to work on government priorities and uplift digital capabilities through digital talent mobility.
           </p>
           <h2>What the program does</h2>
           <p>
-            oneAPS Opportunities gives managers access to digital
-            professions from across the APS. This will assist with
-            short-term flexible work placements, called opporutnities, while
-            staying linked to their home teamsagencies. These forms of
-            placements are supported in the APS Mobility Framework called
-            "Informal Arrangements".
-              </p>
+            oneAPS Opportunities gives managers access to digital professions from across the APS. This will assist with short-term flexible work placements, called opportunities, while staying linked to their home teams/agencies. These forms of placements are supported in the APS Mobility Framework called "Informal arrangements".
+          </p>
+          <h3>How it works</h3>
+          <p>
+            An opportunity is like a ‘micro-assignment’ where APS staff can work on a task, in a role, or on a project for a short period of time.
+          </p>
+          <p>
+            There are many ways to approach this.
+          </p>
+          <p>
+            Employees often move locations as part of these arrangements, but it’s also possible for moves to be virtual.
+          </p>
+          <p>
+            The supervisor and employee will agree on what works best for them.
+          </p>
+          <img
+            className="au-responsive-media-img"
+            src="/calendar.jpg"
+            alt="Work calendar"
+          ></img>
         </div>
       </>
     </DefaultLayout>

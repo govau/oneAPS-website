7ffb06bb5804519a88b11ce32353eaac996e8c7c
malmas18 <malmas18@users.noreply.github.com>
2021-06-11 14:46:34 +1000

    update ga

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -49,7 +49,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "G-3HQK4D29HY",
+        trackingId: "UA-61222473-45",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: true,
         // Avoids sending pageview hits from custom paths

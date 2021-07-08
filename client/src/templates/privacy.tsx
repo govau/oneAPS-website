3a55a3979fd95d3f53d2775355c1c5c2a1c0e3f1
tobiasbrown <2645932+tobiasbrown@users.noreply.github.com>
2021-07-08 16:28:13 +1000

    Add privacy template for privacy policy (#28)
    * Add template for privacy policy
    
    * Add help template filter to match on files with help-pages in path

--- client/src/templates/privacy.tsx
+++ client/src/templates/privacy.tsx
@@ -1,22 +1,20 @@
 import * as React from "react";
+import { Link, graphql, useStaticQuery } from "gatsby";
 import DefaultLayout from "../components/layouts/default-layout";
 import SEO from "../components/seo";
 import { PageContext } from "../types/types";
-import { Link, graphql, useStaticQuery } from "gatsby";
-import { AuSideNav } from "../types/auds";
 
 // markup
-const Help: React.FC<PageContext> = ({ pageContext, location }) => {
+const Privacy: React.FC<PageContext> = ({ pageContext, location }) => {
 
   const data = useStaticQuery(graphql`
-      query MyQuery {
+      query PrivacyQuery {
         allMarkdownRemark(
           filter: {
             fileAbsolutePath: {
-              regex: "/help-pages/"
+              regex: "/privacy/"
             }
           },
-          sort: {fields: fields___slug}
         ) {
           nodes {
             fields {
@@ -31,38 +29,24 @@ const Help: React.FC<PageContext> = ({ pageContext, location }) => {
           }
         }
       }
-  
     `);
+
   const { allMarkdownRemark } = data;
   const { nodes } = allMarkdownRemark;
   let frontmatter;
   let html;
   let tableOfContents;
-  let links = [];
 
   for (const node of nodes) {
     let active = false;
+
     if (location.pathname.includes(node.fields.slug) || node.fields.slug.includes(location.pathname)) {
       frontmatter = node.frontmatter;
       html = node.html;
       tableOfContents = node.tableOfContents
       active = true;
     }
-    links.push({
-      link: node.fields.slug,
-      text: node.frontmatter.title,
-      active
-    });
   }
-  links = links.sort((p1, p2) => {
-    if (p1.link < p2.link) {
-      return -1;
-    }
-    if (p1.link > p2.link) {
-      return 1;
-    }
-    return 0;
-  });
 
   return (
     <DefaultLayout pageContext={pageContext} location={location}>
@@ -73,23 +57,13 @@ const Help: React.FC<PageContext> = ({ pageContext, location }) => {
             <li>
               <Link to="/">Home</Link>
             </li>
-            <li>
-              <Link to="/help-pages/1-about-oneaps/">About oneAPS</Link>
-            </li>
             <li>{frontmatter.title}</li>
           </ul>
         </nav>
         <div className="au-grid" style={{marginTop: '3em'}}>
           <div className="container">
             <div className="row">
-              <div className="about col-sm-12 col-md-4">
-                <AuSideNav linkComponent={i => <Link to={i.to}>{i.children}</Link>}
-                  accordionHeader="In this section"
-                  menuHeaderLink="/help-pages/1-about-oneaps/"
-                  menuHeader="About oneAPS Opportunities"
-                  items={links} />
-              </div>
-              <div className="col-sm-12 col-md-8">
+              <div className="col-sm-12">
                 <h1>{frontmatter.title}</h1><br />
                 <div
                   className="help-page-content"
@@ -104,4 +78,4 @@ const Help: React.FC<PageContext> = ({ pageContext, location }) => {
   );
 };
 
-export default Help;
+export default Privacy;

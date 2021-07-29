d5b2930a6e468b9c795ef81a5d347fc4092a8ad7
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2021-07-29 16:03:34 +1000

    Load agencies from JSON file rather than API
    This is a hack to fix an issue that some users are experiencing where the call to /api/lookup?name=agency results in a 404. Issue needs to be investigated further.

--- client/src/services/lookupService.ts
+++ client/src/services/lookupService.ts
@@ -1,8 +1,15 @@
 import axios from "axios";
+import agencies from "../../content/agencies.json";
 
 export type lookupType = 'agency' | 'opportunitystatus' | 'skills' | 'securityclearance';
 
 export const loadLookup = async (name: lookupType) => {
+  if (name === 'agency') {
+    return Promise.resolve({
+      data: agencies
+    })
+  }
+
   return await axios.get(`/api/lookup`, {
     params: {
       name,

e0dfeb40c57feecd921828ed8a83f7b1c3372e78
Tobias Brown <2645932+tobiasbrown@users.noreply.github.com>
2021-08-13 16:01:14 +1000

    Update guidance content

--- client/src/pages/opportunity-guidance.tsx
+++ client/src/pages/opportunity-guidance.tsx
@@ -20,28 +20,18 @@ const OpportunityGuidancePage: React.FC<PageContext> = ({
             <li>Post an opportunity</li>
           </ul>
         </nav>
-        <h1>Post an opportunity</h1>
         <section>
-          <h2 id="overview">Overview</h2>
-          <p>
-            We are trialling a new approach to mobility in government. Here's some
-            information to help you decide if this is the right place for you.
-          </p>
-        </section>
-        <section>
-          <h2 id="who-can-post-an-opportunity">Who can post an opportunity?</h2>
+          <h1>Post an opportunity</h1>
+          <h2 className="au-display-md">We are trialling a new approach to mobility</h2>
           <p>Any Commonwealth government employee can post an opportunity.</p>
-        </section>
-        <section>
-          <h2 id="what-opportunities-can-I-post">What opportunities can I post?</h2> 
+          <p>An opportunity is like a 'micro-assignment' where staff can work on a task, 
+            in a role or on a program for a short period of time.</p>
           <p>We encourage opportunities that are:</p>
           <ul className="accepted-list">
             <li>A few hours or days to a few months long</li>
             <li>Seeking digital or data skills</li>
-            <li>Able to be completed flexibly through remote working or part-time hours</li>
-            <li>Do not need full onboarding to your organisation’s ICT systems</li>
-            <li>Low risk</li>
-            <li>Able to be completed through an informal arrangement</li>
+            <li>Able to be completed flexibly by remote working or part-time</li>
+            <li>Do not need onboarding to your organisation’s ICT systems</li>
           </ul>
           <p>The following opportunities do not fit the program:</p>
           <ul className="not-accepted-list">
@@ -52,25 +42,12 @@ const OpportunityGuidancePage: React.FC<PageContext> = ({
           </ul>
         </section>
         <section>
-          <h2 id="how-it-works">How it works</h2>
-          <p>An opportunity is kind of like a 'micro-assignment' where APS staff can work on a task, in a role, or on a project for a short period of time.</p>
-          <p>Informal arrangements are usually supported by an email agreement between a host manager, home manager and staff member.</p>
-          <p>Under an informal arrangement:</p>
-          <ul>
-            <li>Staff retain pay and conditions in line with the home organisation’s enterprise agreement</li>
-            <li>Staff remain on home organisation's payroll</li>
-            <li>Staff can work between the organisations (e.g. a two-three day split, or 'week on week off')</li>
-          </ul>
-        </section>
-        <section>
-          <h2 id="staff-member-time-examples">Examples of different ways you might access a staff member's time across a month</h2>
-          <img alt="Simple monthly calendar showing how opportunities can be split between regular duties." src="/calendar.jpeg" />
+          <h2 id="how-it-works" className="au-display-md">How it works</h2>
           <p>For more information, see <Link to="/help-pages/3-creating-an-opportunity/">Creating an opportunity</Link></p>
         </section>
         <section>
-          <h2 id="shape-the-future">Help us shape the future of this initiative</h2>
-          <p>As this is a pilot, we will have a few touchpoints along the way to get your feedback and provide support.</p>
-          <p>We will also encourage you to share your story when the opportunity is completed.</p>
+          <h2 id="shape-the-future" className="au-display-md">Help us shape the future of this initiative</h2>
+          <p>We will get in touch to seek your feedback and provide support.</p>
           <Link className="au-btn margin-top-1" to="/post-opportunity">Continue</Link>
         </section>
       </>
